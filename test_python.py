4f73adf1475f7f18074c2e906d0f646da742a8b1
Jasmin <jaskozjasmin@gmail.com>
2024-12-07 11:01:29 -0500

    add new line

--- test_python.py
+++ test_python.py
@@ -1,8 +1,10 @@
 someNumber = 10
 
-if(someNumber >= 0):
+if(someNumber > 0):
     print("Over zerro")
 elif(someNumber < 0):
     print("Bellow zerro")
+elif(someNumber == 0):
+    print("zerro")    
 else:
-    print("zerro")
+    print(" ")
